4b7b1ff3ad61d70f1f5826f4dbda84969f7e2d72
72umesh <umeshpal8141@gmail.com>
2025-07-01 11:33:57 +0530

    Updated vite url in Axios

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 const instance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://petsellingsystem.onrender.com/api",
+      : `${BASE_URL}/api`,
   withCredentials: true,
 });
 
