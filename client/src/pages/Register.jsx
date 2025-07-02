e35e76e4823c41a477a2f8186f1106d41550090b
72umesh <umeshpal8141@gmail.com>
2025-07-02 12:38:19 +0530

    Updated Backend Url

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -24,7 +24,7 @@ function Register() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/register",
+        "https://petsellingsystem.onrender.com/api/register",
         formData
       );
       alert("Registered successfully!");
