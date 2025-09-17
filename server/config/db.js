15864586f91b180bbb565a7a82a355d43306d284
72umesh <umeshpal8141@gmail.com>
2025-09-18 01:05:40 +0530

    Removed pem file

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,7 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
-import fs from "fs";
 
-import path from "path";
-import { fileURLToPath } from "url";
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const caPath = path.join(__dirname, "isgrootx1.pem");
 
 export const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -18,6 +10,6 @@ export const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
-    ca: fs.readFileSync(caPath),
+    ca: process.env.DB_SSL_CA,
   },
 });
