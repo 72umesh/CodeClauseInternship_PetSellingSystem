94c0603d855c5b82ba6ac7aa0fc24a7581c01636
72umesh <umeshpal8141@gmail.com>
2025-09-18 10:30:06 +0530

    Removed SSL

--- server/config/db.js
+++ server/config/db.js
@@ -9,7 +9,4 @@ export const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    ca: process.env.DB_SSL_CA?.replace(/\\n/g, "\n"),
-  },
 });
